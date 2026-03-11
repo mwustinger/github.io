2e1ca1afb8904242b80a8ab575da1c88dbc7bfa8
Martin Wustinger <Wustinger.Martin@gmail.com>
2026-03-11 22:41:01 +0100

    Update CV

--- assets/js/restrict.js
+++ assets/js/restrict.js
@@ -3,15 +3,7 @@
 // List of allowed paths
 const allowedPaths = ['/', '/about', '/publications', '/cv'];
 
-// Get current path in lowercase
-let path = window.location.pathname.toLowerCase();
-
-// Redirect /cv to /resume
-if (path === '/resume' || path === '/resume/') {
-  window.location.href = '/cv';
-}
-
 // If path is not allowed, redirect to homepage
-else if (!allowedPaths.includes(path)) {
+if (!allowedPaths.includes(path)) {
   window.location.href = '/';
 }
